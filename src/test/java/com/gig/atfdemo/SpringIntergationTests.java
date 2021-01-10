f4cc8567fb3bb63750e3423b5471d49031fe59ed
Orlando Garcia <orlando.garcia@gmail.com>
2021-01-10 11:25:47 -0500

    Removes default spring test to check context loading

--- src/test/java/com/gig/atfdemo/SpringIntergationTests.java
+++ src/test/java/com/gig/atfdemo/SpringIntergationTests.java
@@ -7,9 +7,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @CucumberContextConfiguration
 @SpringBootTest
 class SpringIntergationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
