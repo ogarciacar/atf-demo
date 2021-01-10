17899697f1c4225c8594e3dad66dafc7c1ead5b8
Orlando Garcia <orlando.garcia@gmail.com>
2021-01-10 11:54:30 -0500

    Creates stepdefs input adapter

---
+++ src/test/java/com/gig/atfdemo/car/adapters/bdd/RegisterUserStepdefs.java
@@ -0,0 +1,38 @@
+package com.gig.atfdemo.car.adapters.bdd;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class RegisterUserStepdefs {
+
+    @Given("the user types only its email {string}")
+    public void the_user_types_only_its_email(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user gets an error {int} and the message {string}")
+    public void the_user_gets_an_error_and_the_message(Integer int1, String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user tries to register into the system")
+    public void the_user_tries_to_register_into_the_system() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the user types email {string} and password {string}")
+    public void the_user_types_email_and_password(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user gets a successful registration along with a token")
+    public void the_user_gets_a_successful_registration_along_with_a_token() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
