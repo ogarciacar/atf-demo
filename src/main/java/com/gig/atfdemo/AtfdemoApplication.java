15ce3d01ee59114c23000fda92de060622439eab
Orlando Garcia <orlando.garcia@gmail.com>
2021-01-10 11:19:18 -0500

    Project directories

---
+++ src/main/java/com/gig/atfdemo/AtfdemoApplication.java
@@ -0,0 +1,13 @@
+package com.gig.atfdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtfdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtfdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gig/atfdemo/AtfdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gig.atfdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtfdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
