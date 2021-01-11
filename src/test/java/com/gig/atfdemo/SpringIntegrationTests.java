24695da4d5e54eeac4e23f2a2cc6acd6e9d935b9
Orlando Garcia <orlando.garcia@gmail.com>
2021-01-11 03:53:04 -0500

    Adds dependencies, properties files and config to enable Kafka Support

--- src/test/java/com/gig/atfdemo/SpringIntegrationTests.java
+++ src/test/java/com/gig/atfdemo/SpringIntegrationTests.java
@@ -3,11 +3,15 @@ package com.gig.atfdemo;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.test.context.TestPropertySource;
 
 @CucumberContextConfiguration
 public class SpringIntegrationTests {
 
     @TestConfiguration
+    @PropertySource(name = "kafkaProperties", value = "application.properties")
     @ComponentScan("com.gig.atfdemo")
     public static  class Configuration {
 
